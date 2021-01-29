4859feee73d34fd22c3bd3df67a56d1b65a96e04
lshjhf <lshjsh@hotmail.com>
2021-01-30 01:23:43 +0800

    a

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,6 +1,6 @@
 public class Planet {
-    double xxPos;double yyPos;double xxVel;double yyVel;double mass;String imgFileName;
-    Planet p;
+    public double xxPos;public double yyPos;public double xxVel;public double yyVel;public double mass;public String imgFileName;
+
     private static final double G = 6.67e-11;
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img){
